7cc45eb5523317c4fe52d950beefb416263557b1
Raihan Sharif <raihansharif92@gmail.com>
2025-08-13 13:38:46 +0100

    cart correctly displays product info, count and total price for item * count

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,32 @@
 import { useOutletContext } from "react-router-dom";
 
 function Cart() {
-  const { porductList, selectedItems, setSelectedItems } = useOutletContext();
+  const { productList, selectedItems, setSelectedItems } = useOutletContext();
+  let cartItemSummary = null;
+
+  // populate the cartItemSummary with {product: {product}, count: int, price: float(count * product price)}
+  function generateCartSummary() {
+    return selectedItems.map((cartItem) => {
+      const itemObj = productList.find((item) => item.id === cartItem.id);
+      return {
+        item: itemObj,
+        count: cartItem.count,
+        price: cartItem.count * itemObj.price,
+      };
+    });
+  }
+  cartItemSummary = generateCartSummary();
   return (
     <>
       <main>
-        {selectedItems.map((item) => {
+        {cartItemSummary.map((element) => {
           return (
-            <p>
-              item ID: {item.id} || count: {item.count}
-            </p>
+            <div key={element.item.id}>
+              <p>
+                item ID: {element.item.id} -||- name: {element.item.title} -||-
+                count: {element.count} -||- price: £{element.price}
+              </p>
+            </div>
           );
         })}
       </main>
