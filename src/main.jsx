ced0893b45251889070f11895961d3605e061804
Raihan Sharif <raihansharif92@gmail.com>
2025-08-13 18:52:57 +0100

    basic global css, sticky footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
+import "./main.css";
 
 const router = createBrowserRouter(routes);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
